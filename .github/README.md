198b68bf5aff84bd26aeb11060aae9a13ab56a3a
Jihun Kwon <tjgo103@gmail.com>
2021-06-27 15:48:26 +0900

    change README.md, zsh.sh

--- .github/README.md
+++ .github/README.md
@@ -13,7 +13,6 @@ next step: add, commit, push, ...
 
 ### Clone to new box
 ```
-echo ".dotfiles" >> .gitignore
 git clone --bare https://github.com/kwonj/dotfiles.git $HOME/.dotfiles
 alias dotgit='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 dotgit config --local status.showUntrackedFiles no
--- .scripts/zsh.sh
+++ .scripts/zsh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
